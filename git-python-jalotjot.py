4c934a0e29d0346e428605086c06102ff4962c50
JalotjotRisshaAngeline <jalotjotrisshaangeline@gmail.com>
2022-03-15 13:33:49 +0800

    My Educational Background added

--- git-python-jalotjot.py
+++ git-python-jalotjot.py
@@ -1,3 +1,6 @@
 print ("Rissha Angeline Jalotjot")
 print ("Catubugan, Boac, Marinduque")
 print ("July 4, 2001")
+print ("Marinduque State College")
+print ("Bachelor of Science in Information Technology")
+print ("2nd year - Section A")
